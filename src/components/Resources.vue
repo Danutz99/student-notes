2cb909ab42ab9b96de2b4b01048fa997f28439eb
Danutz99 <danmamaliga2@gmail.com>
2020-11-20 00:37:04 +0200

    renamed from available to recomanded courses

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container grid-list-lg>
         <v-layout row>
-            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Available Courses</v-flex>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Recomanded Courses</v-flex>
         </v-layout>
         <v-layout row wrap>
             <v-flex xs12 sm12 md4>
